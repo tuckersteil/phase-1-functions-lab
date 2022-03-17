729afcfe4de02854933502735248bcb1dbc6bef4
Tucker Steil <tuckersteil@gmail.com>
2022-03-17 13:00:56 -0700

    commit tree 6572d49709eb2db3e943a01ce85a9fc120c397df

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blocks){
+    if (blocks >= 42)
+        return blocks - 42 
+    else 
+        return 42 - blocks
+}
+distanceFromHqInBlocks()
+
+function distanceFromHqInFeet (feet){
+    return distanceFromHqInBlocks (feet) * 264;
+}
+distanceFromHqInFeet()
+
+function distanceTravelledInFeet (start, destination) {
+    if (start < destination)
+        return (destination - start) * 264
+    else 
+        return (start - destination) *264 
+}
+distanceTravelledInFeet ()
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet (start, destination) > 2500)
+        return 'cannot travel that far'
+    else if (distanceTravelledInFeet (start, destination) > 2000)
+        return 25
+    else if (distanceTravelledInFeet (start, destination) <= 400)
+        return 0
+    else if (distanceTravelledInFeet (start, destination) < 2000)
+        return (distanceTravelledInFeet (start, destination) - 400) * .02
+}
